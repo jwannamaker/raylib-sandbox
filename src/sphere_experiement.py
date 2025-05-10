541aee00c11d20a0b72828fedd0395f4067369c6
Johnny Wannamaker <j.wannamaker19@gmail.com>
2025-05-09 18:39:03 -0700

    Update to not use wildcard import

--- src/sphere_experiement.py
+++ src/sphere_experiement.py
@@ -1,28 +1,28 @@
-from pyray import *
+# from pyray import *
+import pyray as pr
 
+pr.init_window(900, 900, "Raylib Experiment - Sphere")
 
-init_window(900, 900, "Raylib Experiment - Sphere")
-
-camera = Camera3D()
-camera.position = Vector3(10.0, 10.0, 10.0)
-camera.target = Vector3(0.0, 0.0, 0.0)
-camera.up = Vector3(0.0, 1.0, 0.0)
+camera = pr.Camera3D()
+camera.position = pr.Vector3(10.0, 10.0, 10.0)
+camera.target = pr.Vector3(0.0, 0.0, 0.0)
+camera.up = pr.Vector3(0.0, 1.0, 0.0)
 camera.fovy = 45.0
-camera.projection = CAMERA_PERSPECTIVE
+camera.projection = pr.CameraProjection.CAMERA_PERSPECTIVE
 
-model_position = Vector3(0.0, 0.0, 0.0)
+model_position = pr.Vector3(0.0, 0.0, 0.0)
 
-disable_cursor()
-set_target_fps(60)
+pr.disable_cursor()
+pr.set_target_fps(60)
 
-while not window_should_close():
-    update_camera(camera, CAMERA_FREE)
-    clear_background(BLACK)
-    
-    begin_drawing()
-    begin_mode_3d(camera)
-    draw_sphere_wires(model_position, 5.0, 20, 10, PURPLE)
-    end_mode_3d()
-    end_drawing()
-    
-close_window()
+while not pr.window_should_close():
+    pr.update_camera(camera, pr.CameraMode.CAMERA_FREE)
+    pr.clear_background(pr.BLACK)
+
+    pr.begin_drawing()
+    pr.begin_mode_3d(camera)
+    pr.draw_sphere_wires(model_position, 5.0, 20, 10, pr.PURPLE)
+    pr.end_mode_3d()
+    pr.end_drawing()
+
+pr.close_window()
